dbce6622a8d78e6f00b6efa1b2df2514501ac35d
Mykola Kovalskyi <112631611+mykolakovalskyi@users.noreply.github.com>
2023-07-15 18:16:03 +0200

    Update getData.jsx

--- src/Api/getData.jsx
+++ src/Api/getData.jsx
@@ -2,7 +2,6 @@ import axios from 'axios';
 
 const API_KEY = '510d6b02ed225a51e009d13f1b3c4d4c';
 const BASE_URL = 'https://api.themoviedb.org';
-///const options = { headers: { accept: 'application/json' } };
 
 const instance = axios.create({
   baseURL: BASE_URL,
